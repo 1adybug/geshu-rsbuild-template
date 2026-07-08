f1f17f4f26722fd80dc7568f62d4c4669f0b11e1
陆子虚 <lurongv@qq.com>
2026-07-08 16:09:41 +0800

    refactor: 简化 useLogin 和 useSendCaptcha 钩子的实现，移除不必要的参数和逻辑

--- hooks/useLogin.ts
+++ hooks/useLogin.ts
@@ -1,34 +1,26 @@
 import { useId } from "react"
 
-import { type UseMutationOptions, useMutation } from "@tanstack/react-query"
+import { createUseMutation } from "soda-tanstack-query"
 
 import { login } from "@/apis/login"
 
 import { cookieStorage } from "@/utils/cookieStorage"
 
-export interface UseLoginParams<TOnMutateResult = unknown> extends Omit<
-    UseMutationOptions<Awaited<ReturnType<typeof login>>, Error, Parameters<typeof login>[0], TOnMutateResult>,
-    "mutationFn"
-> {}
-
-export function useLogin<TOnMutateResult = unknown>({ onMutate, onSuccess, onError, onSettled, ...rest }: UseLoginParams<TOnMutateResult> = {}) {
+export const useLogin = createUseMutation(() => {
     const key = useId()
 
-    return useMutation({
+    return {
         mutationFn: login,
-        onMutate(variables, context) {
+        onMutate() {
             message.open({
                 key,
                 type: "loading",
                 content: `登录中...`,
                 duration: 0,
             })
-
-            return onMutate?.(variables, context) as TOnMutateResult | Promise<TOnMutateResult>
         },
         onSuccess(data, variables, onMutateResult, context) {
             cookieStorage.setItem("token", data.token)
-
             context.client.invalidateQueries({ queryKey: ["get-account", undefined] })
 
             message.open({
@@ -36,17 +28,9 @@ export function useLogin<TOnMutateResult = unknown>({ onMutate, onSuccess, onErr
                 type: "success",
                 content: `登录成功`,
             })
-
-            return onSuccess?.(data, variables, onMutateResult, context)
         },
-        onError(error, variables, onMutateResult, context) {
+        onError() {
             message.destroy(key)
-
-            return onError?.(error, variables, onMutateResult, context)
         },
-        onSettled(data, error, variables, onMutateResult, context) {
-            return onSettled?.(data, error, variables, onMutateResult, context)
-        },
-        ...rest,
-    })
-}
+    }
+})
--- hooks/useSendCaptcha.ts
+++ hooks/useSendCaptcha.ts
@@ -1,37 +1,23 @@
 import { useId } from "react"
 
-import { type UseMutationOptions, useMutation } from "@tanstack/react-query"
+import { createUseMutation } from "soda-tanstack-query"
 
 import { sendCaptcha } from "@/apis/sendCaptcha"
 
-export interface UseSendCaptchaParams<TOnMutateResult = unknown> extends Omit<
-    UseMutationOptions<Awaited<ReturnType<typeof sendCaptcha>>, Error, Parameters<typeof sendCaptcha>[0], TOnMutateResult>,
-    "mutationFn"
-> {}
-
-export function useSendCaptcha<TOnMutateResult = unknown>({ onMutate, onSuccess, onError, onSettled, ...rest }: UseSendCaptchaParams<TOnMutateResult> = {}) {
+export const useSendCaptcha = createUseMutation(() => {
     const key = useId()
 
-    return useMutation({
+    return {
         mutationFn: sendCaptcha,
-        onMutate(variables, context) {
-            return onMutate?.(variables, context) as TOnMutateResult | Promise<TOnMutateResult>
-        },
-        onSuccess(data, variables, onMutateResult, context) {
+        onSuccess(data) {
             message.open({
                 key,
                 type: "success",
                 content: `验证码已发送至 ${data.phone}`,
             })
-
-            return onSuccess?.(data, variables, onMutateResult, context)
         },
-        onError(error, variables, onMutateResult, context) {
-            return onError?.(error, variables, onMutateResult, context)
+        onError() {
+            message.destroy(key)
         },
-        onSettled(data, error, variables, onMutateResult, context) {
-            return onSettled?.(data, error, variables, onMutateResult, context)
-        },
-        ...rest,
-    })
-}
+    }
+})
